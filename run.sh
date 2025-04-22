99a9ce24faa6fa913ad23b56e4d20263ce95b810
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-04-22 06:24:59 -0400

    Verify PR has not changed since workflow started

--- run.sh
+++ run.sh
@@ -13,8 +13,20 @@ fi
 # Get PR number.
 N="$(expr "$GITHUB_REF" : '.*-\([0-9]\+\)-[^-]*$')"
 
-# Get PR base sha using `gh`.
-PR_BASE_SHA="$(gh api /repos/"$GITHUB_REPOSITORY"/pulls/"$N" | jq -r '.base.sha')"
+# Get PR using `gh`.
+PR="$(gh api /repos/"$GITHUB_REPOSITORY"/pulls/"$N")"
+
+# Get PR head sha and base sha.
+set +x
+PR_HEAD_SHA="$(echo "$PR" | jq -r '.head.sha')"
+PR_BASE_SHA="$(echo "$PR" | jq -r '.base.sha')"
+set -x
+
+# Verify PR has not changed since workflow started.
+if ! git diff --quiet "$GITHUB_SHA" "$PR_HEAD_SHA"; then
+    echo "PR has changed since workflow started." >&2
+    exit 1
+fi
 
 # Check whether PR is up to date.
 if git diff --quiet "$GITHUB_EVENT_MERGE_GROUP_BASE_SHA" "$PR_BASE_SHA"; then
